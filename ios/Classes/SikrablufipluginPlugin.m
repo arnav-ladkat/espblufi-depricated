745555c8a2962eda1549fbfbe8286c26effe1b04
arnav-ladkat <arnav@hlth.dev>
2022-08-18 22:08:37 +0530

    removed extra lib

--- ios/Classes/SikrablufipluginPlugin.m
+++ ios/Classes/SikrablufipluginPlugin.m
@@ -12,7 +12,7 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
+  }else {
     result(FlutterMethodNotImplemented);
   }
 }
