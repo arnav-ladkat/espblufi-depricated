5f39c9303ce98f93022431cd156cb9698d3a0d08
arnav-ladkat <arnav@hlth.dev>
2022-08-19 23:19:28 +0530

    Implemented abstract class instead of extending it

--- test/espblufi_test.dart
+++ test/espblufi_test.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:espblufi/models/wifi_devices.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:espblufi/espblufi.dart';
 import 'package:espblufi/espblufi_platform_interface.dart';
@@ -17,6 +18,28 @@ class MockespblufiPlatform
     // TODO: implement sendCustomData
     throw UnimplementedError();
   }
+
+  @override
+  Future<void> configureWifi(String name, String pass) {
+    // TODO: implement configureWifi
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> requestConnection(String deviceID) {
+    // TODO: implement requestConnection
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> scanWifi() {
+    // TODO: implement scanWifi
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement wifiDevices
+  Stream<List<WifiDevices>> get wifiDevices => throw UnimplementedError();
 }
 
 void main() {
